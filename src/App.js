7bdd8a3000085e5f52495cd7a70052535ea33019
shivam <shivampatel27102001@gmail.com>
2021-02-05 19:33:59 +0530

    another state defined

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import './shivam.css';
 function App() {
 
     const [value,setValue] = useState("");
+    const [results,setResults] = useState([]);
+
 
     //8ih3rIf8HXDNuscbkvWx2-1Kj3VboKAoeWXVy13KlX4
 
     const fetchImages = () => {
         fetch(`https://api.unsplash.com/search/photos?client_id=8ih3rIf8HXDNuscbkvWx2-1Kj3VboKAoeWXVy13KlX4&query=${value}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
+        .then(res=>res.json())
+        .then(data=>{
+            //console.log(data);
+            setResults(data.results);
         })
     }
 
@@ -23,6 +26,13 @@ function App() {
             <span>Search</span>
             <input style={{width:"60%"}} type="text" value={value} onChange={(e) => setValue(e.target.value)} />
             <button onClick={()=>fetchImages}>Send</button>
+        </div>
+        <div className="gallery">
+            {
+                results.map((item)=>{
+                    return <image src={item.urls.regular}/>
+                })
+            }
         </div>
     </div>
     );
